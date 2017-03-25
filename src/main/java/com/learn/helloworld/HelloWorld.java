68f153313a440ad88b77cc616f74623228a92c97
SWY\sy <137620821@qq.com>
2017-03-25 22:59:40 +0800

    helloworld

---
+++ src/main/java/com/learn/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.learn.helloworld;
+
+/**
+ * Created by sy on 2017/3/25.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
